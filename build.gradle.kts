a378d60d154b4721546fb49a3fbfbde520b89980
Collin <collinjbarber@gmail.com>
2023-04-08 10:59:55 -0400

    remove __str__ generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.7.20"
-    id("org.jetbrains.intellij") version "1.12.0"
+    id("org.jetbrains.kotlin.jvm") version "1.8.20-Beta"
+    id("org.jetbrains.intellij") version "1.13.1"
 }
 
 group = "me.cjcrafter"
